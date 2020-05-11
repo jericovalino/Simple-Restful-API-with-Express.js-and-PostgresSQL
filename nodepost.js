99a9f1128ca07cd5ecc4af522c75f8d16621109b
Jerico Valino <jericovalino@gmail.com>
2020-05-11 11:41:13 +0800

    .

--- nodepost.js
+++ nodepost.js
@@ -3,21 +3,11 @@ const { Client } = require('pg')
 const client = new Client({
     user: "postgres",
     password: "postgres",
-    database: "tododb"
+    database: "mytododb"
 })
 
 client.connect()
     .then(() => console.log("connected to the database!"))
     .catch(err => console.log(err))
-
-
-    // add rows
-    // 'INSERT INTO todos (title, content) VALUES ($1, $2)', ['sing', 'sing a song']
-
-
-// client.query('SELECT * FROM todos')
-//     .then(res => console.log(res.rows))
-//     .catch(err => console.log(err))
-
 
 module.exports = client;
--- nodexpress.js
+++ nodexpress.js
@@ -10,13 +10,27 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/todo', (req, res) => {
-    res.send("todo");
+    client.query('SELECT * FROM todos')
+        .then(({ rows }) => res.send(rows))
+        .catch(err => console.log(err))
 })
 
 app.post('/api/todo', (req, res) => {
     console.log(req.body.title);
     client.query('INSERT INTO todos (title, content) VALUES ($1, $2)', [req.body.title, req.body.content])
         .then(() => res.send("added"))
+        .catch(err => console.log(err))
+})
+
+app.delete('/api/todo/:id', (req, res) => {
+    client.query(`DELETE FROM todos WHERE id = ${req.params.id}`)
+        .then(res.send("todo with the id of " + req.params.id +" deleted.."))
+        .catch(err => console.log(err))
+})
+
+app.put('/api/todo/:id', (req, res) => {
+    client.query(`UPDATE todos SET content = '${req.body.content}' WHERE id = ${req.params.id}`)
+        .then(() => res.send("updated"))
         .catch(err => console.log(err))
 })
 
